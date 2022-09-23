075f0492ac1ae5975e9ec8b8f5013840a311872a
Pedro Arthur <malpvpandtextures@gmail.com>
2022-09-23 10:53:10 -0300

    Onclick sendo inplementado

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ export default function App(){
     console.log(splitword)
     console.log(wordL)
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
     return<>
     <div class="top">
         <img src={img0}/>
-        <div class="topR"><button>Escolher palavra</button><div class="sublinhados">{splitword.map((w,index)=> <div key={index} class="sublinhado">{w.toUpperCase()}</div>)}</div></div>
+        <div class="topR"><button onClick={()=>letP(splitword.map((w,index)=> <div key={index} class="sublinhado"></div>))}>Escolher palavra</button>
+        <div class="sublinhados">{p}</div></div>
     </div>
     <div class="bot"> 
-    <div class="letras">{alfabeto.map((l,index) => <div key={index} class="letra">{l.toUpperCase()}</div>)}</div>
+    <div class="letras">{alfabeto.map((l,index) => <div key={index} onClick={() => console.log(l)} class="letra">{l.toUpperCase()}</div>)}</div>
     <div class="texto"> Ja sei a palavra <input placeholder=""/> <button>chutar</button> </div>
     </div>
     </>
