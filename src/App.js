3544ff6b9ff0da94ea41eb68411f7473da3401a4
Pedro Arthur <malpvpandtextures@gmail.com>
2022-09-23 10:28:52 -0300

    layout finalizado

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import img0 from "./assets/forca0.png"
+import palavras from "./palavras"
+import { useState } from "react"
+export default function App(){
+    const word = palavras[Math.floor(Math.random() * palavras.length)]
+    const wordL = word.length
+    const[p,letP]= useState("")
+    const splitword = word.split("")
+    console.log(word)
+    console.log(splitword)
+    console.log(wordL)
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    return<>
+    <div class="top">
+        <img src={img0}/>
+        <div class="topR"><button>Escolher palavra</button><div class="sublinhados">{splitword.map((w,index)=> <div key={index} class="sublinhado">{w.toUpperCase()}</div>)}</div></div>
+    </div>
+    <div class="bot"> 
+    <div class="letras">{alfabeto.map((l,index) => <div key={index} class="letra">{l.toUpperCase()}</div>)}</div>
+    <div class="texto"> Ja sei a palavra <input placeholder=""/> <button>chutar</button> </div>
+    </div>
+    </>
+}
